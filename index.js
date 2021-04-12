aba2d33fbf4f74562f22e71b0982acca4b529c97
Redacted <239559@edu.p.lodz.pl>
2021-04-12 11:02:59 +0200

    Add IndexedDB

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+// Database initialization
+
+var db;
+const dbRequest = indexedDB.open("clients", 1);
+
+dbRequest.addEventListener("success", (e) => {
+  db = e.target.result;
+});
+
+dbRequest.addEventListener("upgradeneeded", (e) => {
+  db = e.target.result;
+  db.createObjectStore("clientsStore", {
+    keyPath: "id",
+    autoIncrement: true,
+  });
+});
+
+// Handling form data
+
 const formElem = document.getElementById("form");
 
 formElem.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const data = Object.fromEntries(new FormData(formElem));
-  console.log(data);
+
+  const transaction = db.transaction(["clientsStore"], "readwrite");
+  const store = transaction.objectStore("clientsStore");
+  const addRequest = store.add(data);
+
+  addRequest.addEventListener("success", (e) => {
+    alert("added");
+  });
 });
